bd6890a8e89259d42fa41f90e46aaa6421212b97
amiraghabaglu <95378130+amiraghabaglu@users.noreply.github.com>
2025-03-21 11:40:19 -0700

    Update login_system.cpp

--- login_system.cpp
+++ login_system.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 struct person
 {
-    string username;
-    string password;
+    std::string username;
+    std::string password;
     char side;
     int personCode;
 };
@@ -13,13 +13,13 @@ struct person
 enum side
 {
     RegularUser = 'r',  //This category includes public users who have access to the site but limited features are available to them.
-    Member = 'm',       //These users are registered on the site and have more access than regular users.
+    Member      = 'm',  //These users are registered on the site and have more access than regular users.
     Contributor = 'c',  //These users can create content but usually require approval from administrators.
-    Editor = 'e',       //These users are responsible for reviewing and editing content submitted by authors.
-    Admins = 'a',       //These users have full access to the site and can manage all sections.
-    Developer = 'd',    //These users are responsible for the technical development and maintenance of the site.
-    Security = 's'      //These users are responsible for site security and have special access to manage security.
-    
+    Editor      = 'e',  //These users are responsible for reviewing and editing content submitted by authors.
+    Admins      = 'a',  //These users have full access to the site and can manage all sections.
+    Developer   = 'd',  //These users are responsible for the technical development and maintenance of the site.
+    Security    = 's'   //These users are responsible for site security and have special access to manage security.
+
 };
 
 // Function to display a visual welcome
@@ -46,32 +46,59 @@ void displayInstructions() {
     std::cout << "\n";
 }
 
+// Function to display person information
+void display(person **p, int length)
+{
+    for(int i=0; i<length; i++)
+        cout << "UserName: " << p[i]->username << "\tPassWord: " << p[i]->password << "\tSide: "
+             << p[i]->side << "\t PersonCode: " << p[i]->personCode << endl;
+}
+
 int main() {
-    
-    person p = {"amirbagloo" ,"@Almnv:654" ,'a' ,8447};
-    
-    string username;
-    string password;
-    char side;
-    int personCode;
-    
+
     displayWelcomeMessage();
 
     displayInstructions();
 
-    
+    int length = 7;
+    const int width = 4;
+
+    person **p = new person*[length];
+
+    p[0] = new person({"leilamahmudi" ,"@leila:0990" ,RegularUser ,1379});
+    p[1] = new person({"aminagha" ,"@Amin:0936" ,Member ,1386});
+    p[2] = new person({"mahdismusavi" ,"@Mahdis:0903" ,Contributor ,1386});
+    p[3] = new person({"fatimosavi" ,"@Fati:0936" ,Editor ,1390});
+    p[4] = new person({"amirbagloo" ,"@Almnv:991" ,Admins ,1369});
+    p[5] = new person({"sadrabakal" ,"@Sadra:0477" ,Developer ,1400});
+    p[6] = new person({"alirambakal" ,"@AliRam:0369" ,Security ,1404});
+
+    display(p, length);
+
+    string username;
+    string password;
+    char side;
+    int personCode;
+
+
+
+
     cout << "Enter username:";
     cin >> username;
-    
+
     cout << "Enter password:\n";
     cin >> password;
-    
+
     switch (side) {
-        
+
     }
-        
+
+    for(int i=0; i<length; i++)
+        delete p[i];
+
+    delete[] p;
     cout << "Press a key to exit the program....\n";
-    cin.get(); 
-    
+    cin.get();
+
     return 0;
 }
