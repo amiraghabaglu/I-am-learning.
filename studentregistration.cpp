c73f306a094ada6741c8e9936f8993526e9aa170
amiraghabaglu <95378130+amiraghabaglu@users.noreply.github.com>
2025-03-24 02:40:18 -0700

    Update studentregistration.cpp

--- studentregistration.cpp
+++ studentregistration.cpp
@@ -2,247 +2,206 @@
 
 using namespace std;
 
-struct Student
+struct person
 {
-    string name;
-    string family;
-    int brithday;
-    int gender;
-    string city;
-    string phonnumber;
+  std::string username;
+  std::string password;
+  char side;
+  int personCode;
 };
 
-enum Operat
+enum side
 {
-    Read = 0,
-    Write = 1,
-    Search = 2,
-    Delete ,
-    Modify
+  RegularUser = 'r',  //This category includes public users who have access to the site but limited features are available to them.
+  Member      = 'm',  //These users are registered on the site and have more access than regular users.
+  Contributor = 'c',  //These users can create content but usually require approval from administrators.
+  Editor      = 'e',  //These users are responsible for reviewing and editing content submitted by authors.
+  Admin       = 'a',  //These users have full access to the site and can manage all sections.
+  Developer   = 'd',  //These users are responsible for the technical development and maintenance of the site.
+  Security    = 's'   //These users are responsible for site security and have special access to manage security.
 };
 
-enum Spec
-{
-    Name = 0,
-    Family = 1,
-    Brithday,
-    Gender,
-    City,
-    Phonnum
-};
-
-void print(Student s[10], int i)
-{
-    cout << i << "   " << s[i].name << " " << s[i].family << "\t\t";
-    if(s[i].gender)
-        cout << " mail \t";
-    else
-        cout << " fem  ";
-    cout << s[i].brithday << "\t\t" << s[i].city << "\t\t" << s[i].phonnumber << endl;
+// Function to display a visual welcome
+void displayWelcomeMessage() {
+  std::cout << "******************************************\n";
+  std::cout << "*                                        *\n";
+  std::cout << "*   your welcome to login person system  *\n";
+  std::cout << "*                                        *\n";
+  std::cout << "*          Project: Login System         *\n";
+  std::cout << "*        Author: [Amir Aghabakloo]       *\n";
+  std::cout << "*                                        *\n";
+  std::cout << "******************************************\n";
+  std::cout << "\n";
 }
 
-void inputInfo(Student s[10], int count)
-{
-    cout << "Enter name: ";
-    cin >> s[count].name;
-
-    cout << "Enter family: ";
-    cin >> s[count].family;
-
-    cout << "Enter brithday: ";
-    cin >> s[count].brithday;
-
-    cout << "Enter gender (mail 1 and femail 0): ";
-    cin >> s[count].gender;
-
-    cout << "Enter city:";
-    cin >> s[count].city;
-
-    cout << "Enter phonnumber:";
-    cin >> s[count].phonnumber;
+// Function to display instructions
+void displayInstructions() {
+  std::cout << "Beginner's Guide to Writing a Login System:\n";
+  std::cout << "1. Create a data structure to store user information.\n";
+  std::cout << "2. Request the user's username and password.\n";
+  std::cout << "3. Compare the entered information with the stored data.\n";
+  std::cout << "4. If the information matches, log the user into the system.\n";
+  std::cout << "5. Otherwise, display an error message.\n";
+  std::cout << "\n";
 }
 
-void read(Student s[10] , int count)
+// Function to display person information
+void display(person **p, int length)
 {
-    cout << "ID| Name | Family |  Gender  |  Brith  |   City   |  PhonNumber" << endl;
-    if(count > 10)
-    {
-        print(s, count - 11);
-    }
-    else
-    {
-        for(int i = 0; i < count; i++){
-            print(s, i);
-        }
-    }
-};
-
-int write(Student s[10] ,int count)
-{
-    inputInfo(s, count);
-    count++;
-
-    return count;
-};
-
-void search(Student s[10] , int count)
-{
-    int Spec;
-    
-    string name;
-    string family;
-    int brithday = 0;
-    int gender = 2;
-    string city;
-    string phonnumber;
-
-    cout << "This part of the program is for searching." << endl;
-    cout << "What information do you have?\n"
-    "Enter number 0 from name,\n"
-    "Last name, 1\n"
-    "Date of birth, 2\n"
-    "Gender, 3\n"
-    "City, 4\n"
-    "Contact number, 5" << endl;
-    cin >> Spec;
-
-    if(Spec > 5 || Spec < 0)
-        cout << "You entered the input incorrectly." << endl;
-
-    switch (Spec)
-    {
-    case Name:
-        cout << "enter Name: ";
-        cin >> name;
-    break;
-    case Family:
-        cout << "enter Last name: ";
-        cin >> family;
-    break;
-    case Brithday:
-        cout << "enter Date of birth: ";
-        cin >> brithday;
-    break;
-    case Gender:
-        cout << "enter Gender: ";
-        cin >> gender;
-    break;
-    case City:
-        cout << "enter City: ";
-        cin >> city;
-    break;
-    case Phonnum:
-        cout << "enter Contact number: ";
-        cin >> phonnumber;
-    break;
-    }
-
-    for(int i = 0 ; i < count; i++)
-    {
-        if(name == s[i].name)
-        {
-            read(s, i+11);
-        }
-        else if(family == s[i].family)
-        {
-            read(s, i+11);
-        }
-        else if(brithday == s[i].brithday)
-        {
-            read(s, i+11);
-        }
-        else if(gender == s[i].gender)
-        {
-            read(s, i+11);
-        }
-
-        else if(city == s[i].city)
-        {
-            read(s, i+11);
-        }
-        else if(phonnumber == s[i].phonnumber)
-        {
-            read(s, i+11);
-        }
-    }
+  for(int i=0; i<length; i++)
+    cout << "UserName: " << p[i]->username << "\tPassWord: " << p[i]->password << "\tSide: "
+      << p[i]->side << "\t PersonCode: " << p[i]->personCode << endl;
 }
 
-int deleted(Student s[10] ,int count)
+void regularUser(person **p, int id)
 {
-    std::cout << "Student information deletion section" << std::endl;
+  cout << ".....regularUser yourwelcom....." << endl;
 
-    int id_num;
-    cout << "Give the user ID number from delete:";
-    cin >> id_num;
+  char chois;
+  char order;
+  char comments[50];
 
-    for(int i=id_num; i<count ; i++)
+  cout << "Enter 'c' to comment,\n"
+          "Select order 'o',\n"
+          "Buy cart 'b',\n"
+          "Interest 'l' or 'd',\n"
+          "Exit 'e'." << endl;
+
+  while(true)
+  {
+    cin >> chois;
+    if(chois == 'e')
+      break;
+    switch(chois)
     {
-        s[i].name       = s[i+1].name;
-        s[i].family     = s[i+1].family;
-        s[i].brithday   = s[i+1].brithday;
-        s[i].gender     = s[i+1].gender;
-        s[i].city       = s[i+1].city;
-        s[i].phonnumber = s[i+1].phonnumber;
+      case 'c':
+        cout << "enter your comment:";
+        cin >> comments;
+        cout << "User " << p[id]->username << " comment is: " << comments << endl;
+        break;
+
+      case 'o':
+        cout << "from a to z enter a order: ";
+        cin >> order;
+        break;
+
+      case 'b':
+        int cart;
+        cout << "your order is " << order << endl;
+        cout << "enter info crdit cart.";
+        cin >> cart;
+        cout << "Your order for " << order <<" will arrive in a short while." << endl;
+        break;
+
+      case 'l':
+        cout << "User " << p[id]->username << " liked the product." << endl;
+        break;
+
+      case 'd':
+        cout << "User " << p[id]->username << " disliked the product." << endl;
+        break;
     }
-    count--;
+    cout << "enter comment: ";
+  }
+}
 
-    return count;
-};
-
-void modify(Student s[10] )
+void member()
 {
-    std::cout << "In this section of the program, you can change student details." << std::endl;
+  cout << "member";
+}
+void editor()
+{
+  cout << "editor";
+}
+void admin()
+{
+  cout << "admin";
+}
+void developer()
+{
+  cout << "developer";
+}
+void security()
+{
+  cout << "security";
+}
+void contributor()
+{
+  cout << "contributor";
+}
 
-    int id_num;
-    cout << "Give the user ID number from modify:";
-    cin >> id_num;
+int main() {
 
-    inputInfo(s, id_num);
-};
+  displayWelcomeMessage();
 
-int main(){
-    cout << "A program for storing student information." << endl;
-    Student s[10] = {{"amir","bagloo",1369,1,"zoor","+98936"},
-                     {"amin","agha",1386,1,"zoor","+98937"},
-                     {"lela","mahmud",1379,0,"hasan","+98990"},
-                     {"mads","mosavi",1386,0,"karaj","+98903"},
-                    };
-    int Operat,
-        countiner = 4;
-    while(true)
-    {
-        cout << "Enter 0 to view information,\n"
-                "1 to enter information,\n"
-                "2 to search in information,\n"
-                "3 to delete information,\n"
-                "4 to change information,\n"
-                "5 to exit from program." << endl;
-        cin >> Operat;
-        if(Operat == 5)
+  displayInstructions();
+
+  int length = 7;
+  const int width = 4;
+
+  person **p = new person*[length];
+
+  p[0] = new person({"leilamahmudi" ,"@leila:0990"  ,RegularUser  ,1379});
+  p[1] = new person({"aminagha"     ,"@Amin:0936"  ,Member       ,1386});
+  p[2] = new person({"mahdismusavi" ,"@Mahdis:0903"  ,Contributor  ,1386});
+  p[3] = new person({"fatimosavi"   ,"@Fati:0936"  ,Editor       ,1390});
+  p[4] = new person({"amirbagloo"   ,"@Almnv:991"   ,Admin        ,1369});
+  p[5] = new person({"sadrabakal"   ,"@Sadra:0477"  ,Developer    ,1400});
+  p[6] = new person({"alirambakal"  ,"@AliRam:0369"  ,Security     ,1404});
+
+  string username;
+  string password;
+  char side;
+  int personCode;
+
+  cout << "Enter username: ";
+  cin >> username;
+
+  cout << "Enter password: ";
+  cin >> password;
+
+  for(int i=0; i<7; i++)
+    if(username == p[i]->username)
+      if(password == p[i]->password)
+      {
+        side = p[i]->side;
+        switch (side) {
+          case RegularUser:
+            regularUser(p, i);
             break;
-        else if(Operat > 5 || Operat < 0)
-        {
-            cout << "You entered the input incorrectly." << endl;
-            continue;
+
+          case Member:
+            member();
+            break;
+
+          case Editor:
+            editor();
+            break;
+
+          case Admin:
+            admin();
+            break;
+
+          case Developer:
+            developer();
+            break;
+
+          case Contributor:
+            contributor();
+            break;
+
+          case Security:
+            security();
+            break;
         }
-        switch (Operat)
-        {
-        case Read:
-            read(s, countiner);
-            break;
-        case Write:
-            countiner = write(s, countiner);
-            break;
-        case Search:
-            search(s, countiner);
-            break;
-        case Delete:
-            countiner = deleted(s, countiner);
-            break;
-        case Modify:
-            modify(s);
-            break;
-        }
-    }
-    return 0;
+      }
+
+  for(int i=0; i<length; i++)
+      delete p[i];
+  delete[] p;
+
+  cout << "Press a key to exit the program....\n";
+  cin.get();
+
+  return 0;
 }
